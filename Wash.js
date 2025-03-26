2a31419b19dc2d241978b99518575cc08ab28914
bellsupamongkhol001 <bellsupamongkholm@gmail.com>
2025-03-26 09:55:49 +0700

    Update Wash.js

--- Wash.js
+++ Wash.js
@@ -5,6 +5,7 @@ function filterTable() {
       const washID = row.cells[0].innerText.toLowerCase();
       const employeeID = row.cells[1].innerText.toLowerCase();
       const employeeName = row.cells[2].innerText.toLowerCase();
+      
       if (washID.includes(input) || employeeID.includes(input) || employeeName.includes(input)) {
           row.style.display = '';
       } else {
